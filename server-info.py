47afd1b58d6c948995f7b7334e4ce3d0a6393a25
warzota <aurelien.labord@hotmail.fr>
2023-07-21 10:20:08 +0200

    commit linux1

--- server-info.py
+++ server-info.py
@@ -9,3 +9,4 @@ print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
 print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3))) 
 print("Memory Percent: {:.2f}%".format(memory.percent))
 SyntaxError: multiple statements found while compiling a single statement
+
