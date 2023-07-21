ecc4df401d8e3f657713acad0b6e4f149142f7a8
warzota <aurelien.labord@hotmail.fr>
2023-07-21 10:15:20 +0200

    commit script

---
+++ server-info.py
@@ -0,0 +1,11 @@
+import psutil 
+ 
+# Get the memory usage information 
+memory = psutil.virtual_memory() 
+ 
+# Print the memory usage details 
+print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3))) 
+print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3))) 
+print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3))) 
+print("Memory Percent: {:.2f}%".format(memory.percent))
+SyntaxError: multiple statements found while compiling a single statement
